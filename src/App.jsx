60593ce0977d8c28517db3b98c23c49afcde4d35
vitorfdrangel <vitorfdrangel@gmail.com>
2024-11-04 19:26:29 -0300

    Criando Home

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <div className="container">
+        <Outlet />
+      </div>
     </>
   );
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import classes from "./Home.module.css";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <div className={classes.menu_container}>
+        <h1 className={classes.title}>Nosso Cardápio</h1>
+        <p className={classes.subtitle}>
+          Clique em adicionar para colocar os produtos na sacola de compras. Se
+          preferir, você pode pedir pelo nosso WhatsApp: (xx) XXXXX-XXXX
+        </p>
+      </div>
     </div>
   );
 };
