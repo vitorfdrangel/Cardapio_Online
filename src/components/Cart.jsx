5cde8469cf4d3267266e66e913b09f1dafdda389
vitorfdrangel <vitorfdrangel@gmail.com>
2024-11-16 23:17:57 -0300

    Cart em produção

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,23 @@
-import { Dock } from "react-dock";
+import { useEffect, useState } from "react";
 
-import { MdOutlineClose } from "react-icons/md";
+// hooks
+import { getProductsLs } from "../hooks/useLocalStorage";
+
+import { Dock } from "react-dock";
 
 // Style
 import classes from "./Cart.module.css";
+import { MdOutlineClose } from "react-icons/md";
 
 const Cart = ({ openSidebar, setOpenSidebar }) => {
+  const [orders, setOrders] = useState([]);
+  const [total, setTotal] = useState();
+
+  useEffect(() => {
+    const productsLs = getProductsLs();
+    setOrders(productsLs);
+  }, [openSidebar]);
+
   return (
     <Dock
       position="right"
@@ -20,10 +32,20 @@ const Cart = ({ openSidebar, setOpenSidebar }) => {
         <MdOutlineClose onClick={() => setOpenSidebar(false)} />
         <h1>Meu Pedido</h1>
 
-        <div className={classes.product_list}>
-          <p>Produto 1</p>
-          <p>Produto 2</p>
-        </div>
+        {orders.length === 0 ? (
+          <p>Seu carrinho está vazio!</p>
+        ) : (
+          orders.map((order) => (
+            <div className={classes.product} key={order.name}>
+              <img src={order.img} alt={order.name} />
+              <div className={classes.product_desc}>
+                <p className={classes.product_name}>{order.name}</p>
+                <p>{order.description}</p>
+                <p className={classes.product_price}>{order.price}</p>
+              </div>
+            </div>
+          ))
+        )}
 
         <div>
           <p>
