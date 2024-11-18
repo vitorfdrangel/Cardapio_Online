cd6b20b1eed22c5a0c8e0b522122a9b0b937050f
vitorfdrangel <vitorfdrangel@gmail.com>
2024-11-18 14:59:24 -0300

    Finalizando soma total

--- src/help/data.js
+++ src/help/data.js
@@ -6,26 +6,30 @@ const data = [
   {
     name: "Chesse Burger",
     description: "Carne, queijo, salada e molho especial",
-    price: "16,90",
+    price: "16.90",
     img: cheeseBg,
+    id: "CB",
   },
   {
     name: "X Burger",
     description: "Carne, ovo, queijo, salada e molho especial",
-    price: "18,90",
+    price: "18.90",
     img: megaBg,
+    id: "XB",
   },
   {
     name: "Mega Burger",
     description: "2 Carnes, 2 ovos, queijo, presunto, salada e molho especial",
-    price: "22,90",
+    price: "22.90",
     img: megaBg,
+    id: "MB",
   },
   {
     name: "Triplo Burger",
     description: "3 Carnes, 3 ovos, queijo, presunto, salada e molho especial",
-    price: "26,90",
+    price: "26.90",
     img: megaBg,
+    id: "TB",
   },
 ];
 
